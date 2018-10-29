1ff48bc0db942a78eff19d92e0f5aa0cdc50ff71
Sam Selkregg <sjselkregg@gmail.com>
2018-10-29 14:00:41 -0400

    Added LibGDX Project

---
+++ CSC361_F18_SELKREGG/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.game.CSC_361_SELKREGG;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CSC_361_SELKREGG(), config);
+	}
+}
